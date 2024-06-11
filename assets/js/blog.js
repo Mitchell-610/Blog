976e95912cd7a8f00edf17f5ff1e7702750ea620
Mitchell-610 <Mheit602@gmail.com>
2024-06-11 19:25:08 -0400

    Consts inputed to retreve local storage and section targeted by id, blog js.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Retrieve form data from local storage
+    const blogEntries = localStorage.getItem('blog');
+    const section = document.getElementById(`input`);
 
 });
