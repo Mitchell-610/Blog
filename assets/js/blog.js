12e44261184d6f6f318baf5033230bf5f9ea6a72
Mitchell-610 <Mheit602@gmail.com>
2024-06-11 19:25:48 -0400

    New li made in targeted id section, form js.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,5 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Retrieve form data from local storage
     const blogEntries = localStorage.getItem('blog');
     const section = document.getElementById(`input`);
-
+    const newLi = document.createElement(`li`);
+   
 });
