d4f9347e1670883f970365e8f311e6db2cbebfd3
Mitchell-610 <Mheit602@gmail.com>
2024-06-11 19:43:02 -0400

    Prevent default set in button function along with wildow location href, logic js.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,8 @@
 const backBtn = document.querySelector(`#back`);
 
 backBtn.addEventListener(`click`, function (event) {
-
+    event.preventDefault();
+    window.location.href = `index.html`;
 }
 );
 
