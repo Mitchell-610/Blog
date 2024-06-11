c4643b8641ac737584dbc2a98a76bd3ef1fae35f
Mitchell-610 <Mheit602@gmail.com>
2024-06-11 18:59:10 -0400

    Const username, title, content, and subbmit button applied to form js.

---
+++ assets/js/form.js
@@ -0,0 +1,5 @@
+const userName = document.querySelector(`#Username`);
+const Title = document.querySelector(`#Title`);
+const content = document.querySelector(`#Content`);
+const submitBtn = document.querySelector(`#Submit`);
+
